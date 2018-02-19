409035853cd68e402233db64b96626b2274d0444
L0thric <35493967+L0thric@users.noreply.github.com>
2018-02-19 11:44:26 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<center>
+<div id='main'>
+
+            <link href = "registration.css" type = "text/css" rel = "stylesheet" />    
+            <h2>Contact</h2>    
+            <form name = "form1" action="modified.php" method = "post" enctype = "multipart/form-data" >    
+                <div class = "container">    
+                    <div class = "form_group">    
+                        <label>Nom:</label>    
+                        <input type = "text" name = "fname" value = "" required/>    
+                    </div>    
+                    <div class = "form_group">    
+                        <label>Prenom:</label>    
+                        <input type = "text" name = "mname" value = "" required />    
+                    </div>    
+                    <div class = "form_group">    
+                        <p>importer une ou plusieurs photos:<button type="submit" form="form1" value="Submit">ouvrir</button></p>    
+                          
+                    </div>    
+                    <div class = "form_group">    
+                        <label>Commentaire:</label>    
+                        <input type = "password" name = "pwd" value = "" required/>    
+                    </div>    
+                </div>    
+            </form>  
+</div>			
+        </body>    
+		</center>
+    </html>    
